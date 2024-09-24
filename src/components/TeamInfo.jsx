6856144c8f732de6a82400a1bbe368c700ec358c
Sunchuangyu Huang <62606765+rNLKJA@users.noreply.github.com>
2024-09-24 20:04:00 +1000

    Update TeamInfo.jsx

--- src/components/TeamInfo.jsx
+++ src/components/TeamInfo.jsx
@@ -2,16 +2,16 @@ import Image from "next/image"
 
 const TeamInfo = ({memberName, memberPhoto, memberRole, memberInfo}) => {
 	return (
-		<div>
+		<div className="flex flex-col justify-center">
 			<Image
-				src="/images/test.jpg"
+				src={memberPhoto}
 				alt="member photo"
 				width="1000"
 				height="1000"/>
 			<div className="mt-3">
-				<p className="font-bold">{memberName}</p>
-				<p className="sm:text-base text-sm leading-none">{memberRole}</p>
-				<p className="sm:text-base text-sm leading-none">{memberInfo}</p>
+				<p className="font-bold text-5xl">{memberName}</p>
+				<p className="sm:text-base text-xl leading-none">{memberRole}</p>
+				<p className="sm:text-base text-xl leading-none">{memberInfo}</p>
 			</div>
 		</div>
 
