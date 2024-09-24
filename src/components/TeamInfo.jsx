bcd021d89c465b49cbf4a7e1a5ba2c6721d96e3d
Sunchuangyu Huang <62606765+rNLKJA@users.noreply.github.com>
2024-09-24 20:27:14 +1000

    Update TeamInfo.jsx

--- src/components/TeamInfo.jsx
+++ src/components/TeamInfo.jsx
@@ -5,12 +5,13 @@ import Link from "next/link"
 
 const TeamInfo = ({memberName, memberPhoto, memberRole, memberInfo, memberLinkedin}) => {
 	return (
-		<div className="flex flex-col justify-center">
+		<div className="flex flex-col justify-center ">
 			<Image
 				src={memberPhoto}
 				alt="member photo"
 				width="1000"
-				height="1000"/>
+				height="1000"
+				className="rounded-lg"/>
 			<Link className="mt-3" href={memberLinkedin} target="_blank">
 				<p className="font-bold text-5xl">{memberName}</p>
 				<p className="sm:text-base text-xl leading-none">{memberRole}</p>
